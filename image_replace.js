5a91e23647bf8954231aad6cb4609f5bf0b121f0
DoubleZ <kohei.tashiro@chiba-u.jp>
2020-05-06 23:55:17 +0900

    画像の分類ができる関数を追加

--- image_replace.js
+++ image_replace.js
@@ -8,8 +8,8 @@ function replace_image() {
     });
 };
 
+// ドキュメントの変化を監視
 var observer = new MutationObserver(replace_image);
-
 observer.observe(document, {
     childList: true,
     subtree: true,
@@ -19,3 +19,35 @@ observer.observe(document.documentElement, {
 });
 
 replace_image();
+
+// When the model is loaded
+function modelLoaded() {
+    console.log('Model Loaded!');
+  }
+ 
+const model_url = chrome.runtime.getURL("mobilenet_v1_025_224/model.json")
+
+const classifier = ml5.imageClassifier(model_url, modelLoaded);
+
+function app(set_src) {
+// Initialize the Image Classifier method with MobileNet
+
+    console.log('Loading mobilenet..');
+
+    // 画像分類の実行
+    let imgEl = document.createElement('img');
+    imgEl.id = "img"
+    //CORSに引っかからないように
+    imgEl.crossOrigin = "anonymous"
+    imgEl.src = set_src
+
+    // Make a prediction with a selected image
+    classifier.classify(imgEl, (err, results) => {
+        console.log(results);
+    });
+
+}
+
+app(chrome.runtime.getURL("test.jpg"));
+//app("https://pbs.twimg.com/media/EXU_dDqUMAEP18x?format=jpg&name=900x900");
+//background-image: url("https://pbs.twimg.com/media/EXU_dDqUMAEP18x?format=jpg&name=900x900");
